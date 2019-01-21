f0d644f5a1a61e6746380e6d2245bae526cf1d79
Shaswat Parikh <shaswat.parikh@searshc.com>
2019-01-21 12:06:18 -0600

    [MISC]

--- src/test/java/com/example/springcloudconfig/SpringCloudConfigApplicationTests.java
+++ src/test/java/com/example/springcloudconfig/SpringCloudConfigApplicationTests.java
@@ -9,9 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCloudConfigApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
 
